cec35fd0acb7d472d6febae99481fc9f7a021d59
Fernandeess <matheus.rodrigues@sptech.school>
2022-10-18 14:33:39 -0300

    delete pasta script

--- Site/script/headerSlider.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".nav");
-
-function activeScroll() {
-  menu.classList.toggle("ativo", scrollY > 650);
-  console.log(scrollY)
-}
-
-window.addEventListener("scroll", activeScroll);
