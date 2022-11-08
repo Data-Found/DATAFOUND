ef287ddf5499d696476d28a06c0cb67086a1a6b3
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-08 20:18:48 -0300

    parametrização do login com api

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -19,7 +19,7 @@ function entrar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, email, senha) {
+function cadastrar(nomeUsuario, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function Cadastrar():", nome, email, senha);
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
