7809668b242186d508c5630b236bbe2984218b15
Fernandeess <matheus.rodrigues@sptech.school>
2022-11-10 00:05:12 -0300

    alterações cadastro

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,7 +4,6 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
@@ -20,26 +19,9 @@ function validarSessao() {
 function limparSessao() {
     // aguardar();
     sessionStorage.clear();
-    // finalizarAguardar();
+    finalizarAguardar();
     window.location = "../login.html";
 }
-
-// carregamento (loading)
-// function aguardar() {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "flex";
-// }
-
-// function finalizarAguardar(texto) {
-//     var divAguardar = document.getElementById("div_aguardar");
-//     divAguardar.style.display = "none";
-
-//     var divErrosLogin = document.getElementById("div_erros_login");
-//     if (texto) {
-//         divErrosLogin.innerHTML = texto;
-//     }
-// }
-
 
 // modal
 function mostrarModal() {
